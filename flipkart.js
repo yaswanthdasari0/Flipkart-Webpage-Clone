59faa11e0342bed942c1268dc8061ffe854115b5
yaswanthdasari0 <132220304+yaswanthdasari0@users.noreply.github.com>
2024-05-30 20:00:57 +0530

    Add files via upload

---
+++ flipkart.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.image');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector('.image').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+//Auto-slide functionality
+setInterval(() => {nextSlide();}, 3000);
